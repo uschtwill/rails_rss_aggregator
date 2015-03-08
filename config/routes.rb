4918f076cd5308e8140bb99b6da0cca292058a48
Wilhelm Uschtrin <uschtrinw@gmail.com>
2015-03-08 22:06:52 +0300

    just annotates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,34 @@
 # == Route Map
 #
-#                       Prefix Verb   URI Pattern                                  Controller#Action
-#                         root GET    /                                            entries#index
-#       blogs_update_all_blogs POST   /blogs/update_all_blogs(.:format)            blogs#update_all_blogs
-# blog_download_newest_entries POST   /blogs/:id/download_newest_entries(.:format) blogs#download_newest_entries
-#                        blogs GET    /blogs(.:format)                             blogs#index
-#                              POST   /blogs(.:format)                             blogs#create
-#                     new_blog GET    /blogs/new(.:format)                         blogs#new
-#                    edit_blog GET    /blogs/:id/edit(.:format)                    blogs#edit
-#                         blog GET    /blogs/:id(.:format)                         blogs#show
-#                              PATCH  /blogs/:id(.:format)                         blogs#update
-#                              PUT    /blogs/:id(.:format)                         blogs#update
-#                              DELETE /blogs/:id(.:format)                         blogs#destroy
-#                      entries GET    /entries(.:format)                           entries#index
-#                        entry GET    /entries/:id(.:format)                       entries#show
-#                              DELETE /entries/:id(.:format)                       entries#destroy
+#                   Prefix Verb   URI Pattern                    Controller#Action
+#                     root GET    /                              entries#index
+#                   update POST   /update(.:format)              blogs#update_all_blogs
+#                   newest POST   /newest(.:format)              blogs#newest_entries
+#         new_user_session GET    /users/sign_in(.:format)       sessions#new
+#             user_session POST   /users/sign_in(.:format)       sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)      sessions#destroy
+#            user_password POST   /users/password(.:format)      devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+#                          PATCH  /users/password(.:format)      devise/passwords#update
+#                          PUT    /users/password(.:format)      devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+#        user_registration POST   /users(.:format)               devise/registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+#                          PATCH  /users(.:format)               devise/registrations#update
+#                          PUT    /users(.:format)               devise/registrations#update
+#                          DELETE /users(.:format)               devise/registrations#destroy
+#                  entries GET    /entries(.:format)             entries#index
+#                    entry GET    /entries/:id(.:format)         entries#show
+#                          PATCH  /entries/:id(.:format)         entries#update
+#                          PUT    /entries/:id(.:format)         entries#update
+#                          DELETE /entries/:id(.:format)         entries#destroy
 #
 
 Rails.application.routes.draw do
-  devise_for :users
- root 'entries#index'
+  
+root 'entries#index'
 
 post '/update' => 'blogs#update_all_blogs', as: :update
 post '/newest' => 'blogs#newest_entries', as: :newest
