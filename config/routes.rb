ed9a22bc4ba8b7ef3d51c91c6780d148349ebe2b
Wilhelm Uschtrin <uschtrinw@gmail.com>
2015-03-08 16:58:51 +0300

    fixes blogs_update_all_blogs route

--- config/routes.rb
+++ config/routes.rb
@@ -20,11 +20,12 @@
 Rails.application.routes.draw do
  root 'entries#index'
 
- scope '/blogs' do
-   post '/update_all_blogs' => 'blogs#update_all_blogs', as: :blogs_update_all_blogs
-   post '/:id/download_newest_entries' => 'blogs#download_newest_entries', as: :blog_download_newest_entries
-   post '/download_newest_entries' => 'blogs#download_newest_entries', as: :blogs_download_newest_entries
- end
+post '/update_all_blogs' => 'blogs#update_all_blogs', as: :update_all_blogs
+
+ # scope '/blogs' do
+ #   post '/:id/download_newest_entries' => 'blogs#download_newest_entries', as: :blog_download_newest_entries
+ #   post '/download_newest_entries' => 'blogs#download_newest_entries', as: :blogs_download_newest_entries
+ # end
 
  # resources :entries, only: [:index, :show, :destroy, :update], constraints: {subdomain: 'api'}
 
