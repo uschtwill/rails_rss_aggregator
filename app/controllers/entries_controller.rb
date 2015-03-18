9e8bd822e9a402ac11fa3dcfe8da223b2f79eb6f
dwanjuhi1 <dwanjuhi@alueducation.com>
2015-03-18 18:49:53 +0000

    app now tweets entry titles to NaiTec Account

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,13 @@
 class EntriesController < ApplicationController
 
-  def index
+  def index    
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "OaPIa6c82YEEaFVB8C4WKvWza"
+      config.consumer_secret     = "txb7PCdjv423WIUpBTnGFzMNAPxnt7O69t2k0fDMOjD6J4nXr0"
+      config.access_token        = "3094798869-J4XQ1Ml9H4phtk6fQ5v3df6Go0aPvN8IXkYeplK"
+      config.access_token_secret = "hop2ORt39UtT8JZc94rX5UnGQ0sdwGcjk9Dak3zSmk8tV"
+    end
+    
     @entries = Entry.all.order(published: :desc).page(params[:page]).per(10)
 
     if blog_id = params[:blog_id]
@@ -11,7 +18,9 @@ class EntriesController < ApplicationController
        format.json { render json: @entries, status: 200 }
        format.xml { render json: @entries, status: 200 }
        format.html { render :index }
-    end   
+    end  
+    
+    
     
   end
 
