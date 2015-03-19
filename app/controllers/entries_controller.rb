6776ecabe6f354117b3d44050167d74def674d35
Wilhelm Uschtrin <uschtrinw@gmail.com>
2015-03-19 13:12:56 +0300

    fixes twitter implementation and sluggish index loading by moving the twitter-push-logic to the blog-model

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,6 @@
 class EntriesController < ApplicationController
 
   def index    
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "OaPIa6c82YEEaFVB8C4WKvWza"
-      config.consumer_secret     = "txb7PCdjv423WIUpBTnGFzMNAPxnt7O69t2k0fDMOjD6J4nXr0"
-      config.access_token        = "3094798869-J4XQ1Ml9H4phtk6fQ5v3df6Go0aPvN8IXkYeplK"
-      config.access_token_secret = "hop2ORt39UtT8JZc94rX5UnGQ0sdwGcjk9Dak3zSmk8tV"
-    end
     
     @entries = Entry.all.order(published: :desc).page(params[:page]).per(10)
 
